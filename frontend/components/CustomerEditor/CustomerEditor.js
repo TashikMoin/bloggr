891f3fc054a957ef03709768e7e8bb8680d2a01e
Saif Ul Islam <saifulislam84210@gmail.com>
2021-11-06 07:59:12 +0500

    Removed image component from the default editor
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- frontend/components/CustomerEditor/CustomerEditor.js
+++ frontend/components/CustomerEditor/CustomerEditor.js
@@ -133,19 +133,6 @@ export default function CustomEditor() {
                         text:
                             "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
                     }
-                },
-                {
-                    type: "image",
-                    data: {
-                        file: {
-                            url:
-                                "https://codex.so/upload/redactor_images/o_e48549d1855c7fc1807308dd14990126.jpg"
-                        },
-                        caption: "",
-                        withBorder: true,
-                        stretched: false,
-                        withBackground: false
-                    }
                 }
             ],
             version: "2.12.4"
