f9bef5ea4a9daed577135800fd702f0963ba7eca
TashikMoin <tashikmoinsheikh@gmail.com>
2021-12-12 03:41:27 +0500

    added update request for blog

--- frontend/pages/blog/[id].js
+++ frontend/pages/blog/[id].js
@@ -1,9 +1,30 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import PostStyling from "../styles/Post/Post.module.css"
-
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import jwt from 'jwt-decode'
+import Link from 'next/link';
 
 const Blog = () => {
+
+    const router = useRouter();
+    const { id } = router.query;
+    const [user, setUser] = useState({});
+
+    const [data, setData] = useState({});
  
+    useEffect(() => {
+        if(!id) { return; }
+        const Token = localStorage.getItem('Token');
+        setUser(jwt(Token));
+        const fetchData = async () => { 
+            const Token = localStorage.getItem('Token');
+            await axios.get(`http://localhost:37606/api/blogs/${id}`, { headers: {"Authorization" : `Bearer ${Token}`} })
+            .then((response) => {setData(response.data);})
+            .catch((error) => console.log(error));
+        }  
+        fetchData();
+    }, [id]);
 
   return (
     <div className={PostStyling.container}>
@@ -12,24 +33,39 @@ const Blog = () => {
             <div className={PostStyling.postheader}>
 
                 <div className={PostStyling.imagecontainer}>
-                    <img src="/public/logo.png" alt=""></img>
+                    <img style={{height: '120px', width: '100px'}} src="/blog.png" alt=""></img>
                 </div>
 
                 <div className={PostStyling.headingcontainer}>
-                    <h1 className={PostStyling.heading}> Blog </h1> 
-                    <div className={PostStyling.teachername}> Rauf Ahmed Shams Malick • Oct 31 </div>
-                    {/* teachername + date  //media query to be written */}
+                    <h1 className={PostStyling.heading}> {data.title} </h1> 
+                    <div className={PostStyling.teachername}> <b> Author:</b> {user.Name} </div>
                 </div> 
 
             </div>
 
-            <div className={PostStyling.postcontent}>
-                <p className={PostStyling.content}>
+            <h1> Description </h1>
+            <div className={PostStyling.description}>
+                <p className={PostStyling.descriptionContent}>
+                    {data.description}
                 </p>
             </div>
 
-            
+            <h1> Content </h1>
+            <div className={PostStyling.postcontent}>
+                <p className={PostStyling.content}>
+                    {data.content}
+                </p>
+            </div>
 
+            <div className={PostStyling.buttonContainer}>
+                <Link href={`/update/${id}`}>
+                    <a>
+                        <button className={PostStyling.lbtn}> Update </button> 
+                    </a>
+                </Link> 
+                <button className={PostStyling.rbtn}> Delete </button>
+            </div>
+    
         </div>
     </div>
   );
---
+++ frontend/pages/update/[id].js
@@ -0,0 +1,119 @@
+import React, {useState, useEffect} from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+import Router from "next/router";
+import { useRouter } from 'next/router';
+import styles from "../styles/create.module.css";
+
+import {
+    Container,
+    Grid,
+    TextField
+} from "@material-ui/core";
+
+export default function Update() {
+
+    const [title, setTitle] = useState();
+    const [description, setDescription] = useState();
+    const [content, setContent] = useState();
+    const router = useRouter();
+    const { id } = router.query;
+
+
+
+    useEffect(() => {
+        if(!id) { return; }
+        const fetchData = async () => { 
+            const Token = localStorage.getItem('Token');
+            await axios.get(`http://localhost:37606/api/blogs/${id}`, { headers: {"Authorization" : `Bearer ${Token}`} })
+            .then((response) => {setTitle(response.data.title); setDescription(response.data.description); setContent(response.data.content);})
+            .catch((error) => console.log(error));
+        }  
+        fetchData();
+    }, [id]);
+
+
+
+
+    const updateBlog = (event) => 
+    {
+        event.preventDefault();
+        if (title == "" || description == "" || content == "") 
+        {
+          alert(`Please fill all the required fields!`);
+        } 
+        else 
+        {
+            const formData = {
+            Title: title,
+            Description: description,
+            Content: content
+            };
+            const Token = localStorage.getItem('Token');
+            axios.put(`http://localhost:37606/api/blogs/${id}`, formData, { headers: {"Authorization" : `Bearer ${Token}`} })
+            .then(response => {alert(`Blog Updated!`); Router.push('/feed')} )
+            .catch(error => alert(error));
+        }
+
+    };
+
+    return (
+        <Container className={styles.container}>
+            <Grid container direction="row" justifyContent="flex-start" alignItems="stretch">
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="stretch"
+                >
+                    <Link href="/feed">
+                        <Grid item>
+                            <img src="/chevron-left.png" width="64px" height="64px" />
+                        </Grid>
+                    </Link>
+
+                    <Grid onClick={updateBlog} style={{cursor: 'pointer'}} item className={styles.postButton}>
+                        <span> Update </span>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="flex-start" alignItems="stretch" style={{ paddingTop: "50px" }} >
+                <Grid item xs={12} className={styles.contentShowcase}>
+                    <div className={styles.centerTextField}>
+                        <TextField
+                            id="standard-multiline-static"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            multiline
+                            rows={4}
+                            variant="standard"
+                            className={styles.textFieldStyling}
+                        />
+                    </div>
+                    <div className={styles.centerTextField}>
+                        <TextField
+                            id="standard-multiline-static"
+                            multiline
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            rows={4}
+                            variant="standard"
+                            className={styles.textFieldStyling}
+                        />
+                    </div>
+                    <div className={styles.centerTextField}>
+                        <TextField
+                            id="standard-multiline-static"
+                            multiline
+                            onChange={(e) => setContent(e.target.value)}
+                            value={content}
+                            rows={35}
+                            variant="standard"
+                            className={styles.textFieldStyling}
+                        />
+                    </div>
+                </Grid>
+            </Grid>
+        </Container >
+    );
+};
