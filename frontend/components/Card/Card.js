9b58ab4ed2fc0f242f64be1bc7e28522772da717
Saif Ul Islam <saifulislam84210@gmail.com>
2021-12-22 09:58:36 -0800

    refactored components
    Signed-off-by: Saif Ul Islam <saifulislam84210@gmail.com>

--- frontend/components/Card/Card.js
+++ frontend/components/Card/Card.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import styles from './card.module.css';
+import Link from "next/link";
 
-import { Grid } from '@material-ui/core';
+import styles from "./card.module.css";
 
-export default function CustomCard({ title, description, content }) {
-    return (
-        <Grid container direction="row" justifyContent="space-between" alignItems="stretch" className={styles.cardBox}>
-            <Grid item xs={11} className={styles.rightStyling}>
-                <h1> {title} </h1>
-                <p className={styles.dateStyle}>{description}</p>
-                <p className={styles.contentStyle}>{content}</p>
-            </Grid>
+import { Grid } from "@mui/material";
+
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+export default function Card({ title, description, content, href }) {
+  const [likes, setLikes] = React.useState(Math.floor(Math.random() * 10));
+
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="stretch"
+      className={styles.cardBox}
+    >
+      <Link href={href}>
+        <Grid item xs={10} className={styles.rightStyling}>
+          <h1> {title} </h1>
+          <h2> Saif ul Islam </h2>
+          <p className={styles.dateStyle}>{description}</p>
+          <p className={styles.contentStyle}>{content}</p>
         </Grid>
-    )
+      </Link>
+      <Grid item xs={1} style={{margin: "25px"}}>
+        <FavoriteIcon onClick={() => setLikes(likes + 1)} />
+        <p>{likes}</p>
+      </Grid>
+    </Grid>
+  );
 }
